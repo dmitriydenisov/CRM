511557795310577b37d8a879c66e2c876ca97117
dmitriydenisov <dmitriyd2009@yandex.ru>
2022-04-23 21:34:03 +0400

    реализован навигатор

---
+++ src/core/Navigator.js
@@ -0,0 +1,67 @@
+import Observable from "./Observable.js";
+
+class Navigator extends Observable {
+  $data = {};
+  constructor(handler = () => {}) {
+    super();
+
+    const params = new URLSearchParams(location.search);
+
+    for (const [key, value] of params) {
+      this.$data[key] = value;
+    }
+
+    this.subscribe(handler);
+    this.dispatch(this);
+  }
+
+  has(key) {
+    return this.$data.hasOwnProperty(key);
+  }
+  set(key, value) {
+    //обновляем или добовляем данные в data
+    this.$data = {
+      ...this.$data,
+      [key]: value,
+    };
+
+    const params = new URLSearchParams();
+
+    for (const [key, value] of Object.entries(this.$data)) {
+      params.append(key, value);
+    }
+
+    history.pushState(null, null, `?${params.toString()}`);
+    this.dispatch(this);
+  }
+  get(key, defultValue) {
+    if (this.$data.hasOwnProperty(key)) {
+      return this.$data[key];
+    }
+    return defultValue;
+  }
+  remove(key) {
+    if (!this.$data.hasOwnProperty(key)) {
+      return;
+    }
+
+    delete this.$data[key];
+
+    const params = new URLSearchParams();
+
+    for (const [key, value] of Object.entries(this.$data)) {
+      parms.append(key, value);
+    }
+
+    history.push(null, null, `?${params.toString()}`);
+    this.dispatch(this);
+  }
+  get(key, defultValue) {
+    if (this.$data.hasOwnProperty(key)) {
+      return this.$data[key];
+    }
+    return defultValue;
+  }
+}
+
+export default Navigator;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import Store from "./core/Store.js";
 import OrdersTable from "./core/OrdersTable.js";
 import Paginator from "./core/Paginator.js";
+import Navigator from "./core/Navigator.js";
 
 const store = new Store();
 store.download();
 
 console.log(store);
 
-const ot = new OrdersTable(
+const ordersTable = new OrdersTable(
   document.querySelector('[data-mount="ordersTable"]'),
   store.orders.slice(0, 5) //отобразить первые 5 заказов
 );
 
-ot.on("edit", (orderId) => console.log({ orderId }));
+ordersTable.on("edit", (orderId) => console.log({ orderId }));
 
 const paginator = new Paginator(
   document.querySelector('[data-mount="pagination"]'),
@@ -21,6 +22,13 @@ const paginator = new Paginator(
 );
 
 paginator.on("move", (nextPage) => {
-  paginator.page = nextPage;
-  ot.orders = store.orders.slice((nextPage - 1) * 5, nextPage * 5);
+  // paginator.page = nextPage;
+  // ot.orders = store.orders.slice((nextPage - 1) * 5, nextPage * 5);
+  navigator.set("page", nextPage);
+});
+
+const navigator = new Navigator((navigator) => {
+  const page = parseInt(navigator.get("page", 1), 10);
+  paginator.page = page;
+  ordersTable.orders = store.orders.slice((page - 1) * 5, page * 5);
 });
